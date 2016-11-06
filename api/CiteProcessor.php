36033691d782aba43f7612fcf1686b3c245e696e
Jo Brunner <jo.brunner@mayflower.de>
2016-11-06 14:09:23 +0100

    Extens the Citation Processor fall back implementation.

--- api/CiteProcessor.php
+++ api/CiteProcessor.php
@@ -1,9 +1,12 @@
 <?php
 namespace jobrunner\inlitteris\api;
 
-use jobrunner\inlitteris\helpers\Html;
+//use jobrunner\inlitteris\helpers\Html;
 use jobrunner\inlitteris\api\Citation\Author;
 use jobrunner\inlitteris\api\Citation\Formatter\AuthorFormatter;
+
+use jobrunner\inlitteris\api\Citation\Renderer\Bibliography;
+use jobrunner\inlitteris\api\Citation\Renderer\Bibliography\Book;
 
 /**
  * Simple default CiteProcessor
@@ -13,27 +16,69 @@ use jobrunner\inlitteris\api\Citation\Formatter\AuthorFormatter;
  */
 class CiteProcessor
 {
+    /**
+     * Citeproc-php compat mehtod
+     *
+     * @param null   $csl
+     * @param string $lang
+     */
     public function init($csl = null, $lang = 'en-US')
     {
     }
 
 
+    /**
+     * Renders citation or bibliography
+     *
+     * @param      $reference
+     * @param null $mode    Optional. Rendering mode 'citation' or 'bibliography'. Defaults to bibliography.
+     *
+     * @return string
+     */
     public function render($reference, $mode = null)
     {
         switch ($mode) {
             case 'citation':
                 return $this->renderCitation($reference);
+//                return (new CitationRenderer($reference))->render($reference);
 
             case 'bibliography':
             default:
-                return $this->renderBibliography($reference);
+                return (new Bibliography($reference))->render();
         }
     }
 
 
     public function renderCitation($reference)
     {
-        return "not implemented";
+        $format    = AuthorFormatter::SEQ_FAMILY |
+                     AuthorFormatter::CAPITALIZATION_NORMAL;
+
+        $formatter = new AuthorFormatter($format);
+
+        $authors   = $reference->{'author'};
+
+        $author    = new Author([
+            'givenName'  => $authors[0]->given,
+            'familyName' => $authors[0]->family
+        ]);
+
+        $output = $author->format($formatter);
+
+        if (count($authors) > 1) {
+            // First author et. al.
+            $output = $output . ' et. al.' . $this->renderYear($reference);
+        } elseif (count($authors) > 0) {
+            // Author as is
+        } else {
+            $output = 'Anonymous';
+        }
+
+        $output = "(" . $output . ", " . $this->renderYear($reference) . ")";
+
+        return $output;
+//
+//        return Html::encode($output);
     }
 
 
@@ -51,19 +96,44 @@ class CiteProcessor
                     '– In: ' .
                     $this->renderEditor($reference->{'editor'}) .
                     $this->renderContainerTitle($reference->{'container-title'}) .
-                    $this->renderVolume($reference->{'volume'}) .
-                    $this->renderPlace($reference->{'publisher-place'}) .
-                    $this->renderPublisher($reference->{'publisher'}) .
+                    $this->renderVolume($reference) .
+                    $this->renderPlace($reference) .
+                    $this->renderPublisher($reference) .
                     $this->renderNumberOfPages($reference->{'number-of-pages'}) .
                     '<br>';
             case 'book':
-                return 'not implemented yet<br/>';
+
+//                echo "<pre>";
+//                print_r($reference);
+//                exit;
+
+//                return 'book bibliography not yet implemented<br/>';
+                return ''
+                    . '<b>'
+                    . $this->renderAuthor($reference)
+                    . '('
+                    . $this->renderIssued($reference)
+                    . ')'
+                    . '</b>'
+                    . ': '
+                    . $this->renderTitle($reference)
+                    . ', '
+
+//                    $this->renderPageLocator($reference->{'page'}) .
+//                    '– In: ' .
+//                    $this->renderEditor($reference->{'editor'}) .
+//                    $this->renderContainerTitle($reference->{'container-title'}) .
+                    . $this->renderVolume($reference) .
+                    $this->renderPlace($reference) .
+                    $this->renderPublisher($reference) .
+                    $this->renderNumberOfPages($reference->{'number-of-pages'}) .
+                    '<br>';
 
             case 'article-journal':
                 return
                     '<b>' .
                     $this->renderAuthor($reference->{'author'})          .
-                    $this->renderIssued($reference->{'issued'})          .
+                    $this->renderIssued($reference)          .
                     '</b>' .
                     $this->renderTitle($reference->{'title'})   . '. '   .
                     $this->renderContainerTitle($reference->{'container-title'}) .
@@ -75,6 +145,28 @@ class CiteProcessor
 
 
         return "<li>Reference ({$reference->type})</li>";
+    }
+
+
+    /**
+     * Returns the year of publication or 's. a.' (sine anno = without year)
+     *
+     * @param $reference
+     *
+     * @return string
+     */
+    public function renderYear($reference)
+    {
+        $issued = empty($reference->{'issued'}) ? null : $reference->{'issued'};
+
+        if (empty($issued->{'date-parts'})) {
+
+            return 's. a.';
+        }
+
+        $copy = $issued->{'date-parts'};
+
+        return array_shift($copy)[0];
     }
 
 
@@ -101,11 +193,24 @@ class CiteProcessor
     }
 
 
-    public function renderAuthor(array $authors)
+    /**
+     * Renders Authors for Bibliography
+     *
+     * @param $reference
+     *
+     * @return string
+     */
+    public function renderAuthor($reference)
     {
+        $authors   = empty($reference->{'author'}) ? [] : $reference->{'author'};
+
+        if (empty($authors)) {
+
+            return 'S.a.';
+        }
+
         $output    = [];
         $separator = ' & ';
-
         $format    = AuthorFormatter::SEQ_FAMILY_COMMA_GIVEN |
                      AuthorFormatter::GIVEN_NAME_SECOND |
                      AuthorFormatter::GIVEN_NAME_ABBR_ALL |
@@ -122,39 +227,85 @@ class CiteProcessor
             $output[] = $author->format($formatter);
         }
 
-        return Html::encode(implode($separator, $output));
+        return implode($separator, $output);
     }
 
 
-    public function renderIssued($issued)
+    public function renderIssued($reference)
     {
-        return " (" . Html::encode($issued->literal) . "): ";
+//        $issued = empty($reference->{'issued'}) ? 's. a.' : $reference->{'issued'};
+
+        return " (" . $this->renderYear($reference) . "): ";
+
+//        return " (" . Html::encode($this->renderYear($reference)) . "): ";
     }
 
 
-    public function renderTitle($title)
+    /**
+     * Returns title or s.t. (sensu titulo = without title)
+     *
+     * @param $reference
+     *
+     * @return mixed|string
+     */
+    public function renderTitle($reference)
     {
-        return Html::encodeWithoutEm(rtrim($title, ',. '));
+        $title = empty($reference->{'title'}) ? 'S.t.' : rtrim($reference->{'title'}, ',. ');
+
+        return $title;
+//        return Html::encodeWithoutEm($title);
     }
 
 
-    public function renderContainerTitle($title)
+    public function renderContainerTitle($reference)
     {
-        return $this->renderTitle($title) . '. ';
+        $title = empty($reference->{'title'}) ? '' : rtrim($reference->{'title'}, ',. ');
+
+        return $title . '. ';
+
+//        return Html::encodeWithoutEm($title . '. ');
     }
 
 
-    public function renderEditor(array $editors)
+    /**
+     * Renders the Editor(s) of an Book
+     *
+     * @param $reference
+     *
+     * @return string
+     */
+    public function renderEditor($reference)
     {
-        return $this->renderAuthor($editors) . ' (Eds.): ';
+        $editor = empty($reference->{'editor'}) ? null : $this->renderAuthor($reference->{'editor'}); //  . ' (Eds.): ';
+
+        if (empty($editor)) {
+
+            return 'S.ed.: ';
+        }
+
+        if (count($reference->{'editor'}) > 1) {
+            $eds = ' (Eds.): ';
+        } else {
+            $eds = ' (Ed.): ';
+        }
+
+        return $this->renderAuthor($reference->{'editor'}) . $eds;
     }
 
 
-    public function renderVolume($volume)
+    /**
+     * Renders the Volume of a Book
+     *
+     * @param $reference
+     *
+     * @return string
+     */
+    public function renderVolume($reference)
     {
-        if (!empty($volume)) {
+        if (!empty($reference->{'volume'})) {
 
-            return 'Vol. ' . Html::encode($volume) . '. ';
+            return 'Vol. ' . rtrim($reference->{'volume'}, '., ') . '. ';
+//            return 'Vol. ' . Html::encode($reference->{'volume'}) . '. ';
         }
 
         return '';
@@ -172,25 +323,32 @@ class CiteProcessor
     }
 
 
-    public function renderPlace($place)
+    public function renderPlace($reference)
     {
+        $place = empty($reference->{'publisher-place'}) ? null : $reference->{'publisher-place'};
+
         if (!empty($place)) {
 
-            return Html::encode(rtrim($place, '. ') . '. ');
+//            return Html::encode(rtrim($place, '. ') . '. ');
+            return rtrim($place, '. ') . '. ';
         }
 
-        return '';
+        // sine loco (= _without place_ of publication)
+        return 's. l. ';
     }
 
 
-    public function renderPublisher($publisher)
+    public function renderPublisher($reference)
     {
+        $publisher = empty($reference->{'publisher'}) ? null : $reference->{'publisher'};
+
         if (!empty($publisher)) {
 
-            return Html::encode(rtrim($publisher, '. ') . '. ');
+            return rtrim($publisher, '. ') . '. ';
         }
 
-        return '';
+        // sine nomine (= _without name_ of publisher)
+        return 's. n. ';
     }
 
 
@@ -198,7 +356,8 @@ class CiteProcessor
     {
         if (!empty($numberOfPages)) {
 
-            return Html::encode(rtrim($numberOfPages, '.pf ') . ' pp. ');
+            return rtrim($numberOfPages, '.pf ') . ' pp. ';
+//            return Html::encode(rtrim($numberOfPages, '.pf ') . ' pp. ');
         }
 
         return '';
